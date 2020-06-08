c252efe947b2a3211205de994d7ab0b847505d37
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-06-08 21:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,9 +16,10 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
+  
   let lineOrder = [];
 
-  for (let i = 0; i < katzDeliLine.length; i++) {
+  for (let i = 0; i <= katzDeliLine.length; i++) {
     lineOrder.push(`${i + 1}. ${katzDeliLine[i]}`);
   } return `The line is currently: ${lineOrder.join(', ')}`
 }
