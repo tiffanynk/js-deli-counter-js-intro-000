705525bcb8a8f124bfdfbeeb04863bf7e3c5b455
Tiffany Kanjanabout <tiffanynitk@gmail.com>
2020-06-08 21:39:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function currentLine(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty."
   }
-  
+
   let lineOrder = [];
 
   for (let i = 0; i <= katzDeliLine.length; i++) {
